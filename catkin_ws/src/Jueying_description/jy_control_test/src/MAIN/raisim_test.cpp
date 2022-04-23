b92c4a64bec7060767e9444beb872ad52534e289
Jiyu Yu <351123845@qq.com>
2022-04-24 02:00:18 +0800

    can add jypro urdf to raisim

--- catkin_ws/src/Jueying_description/jy_control_test/src/MAIN/raisim_test.cpp
+++ catkin_ws/src/Jueying_description/jy_control_test/src/MAIN/raisim_test.cpp
@@ -8,28 +8,32 @@ int main(int argc, char* argv[]) {
   raisim::World::setActivationKey(str);
 
   raisim::World world;
-  world.setTimeStep(0.002);
+  world.setTimeStep(0.001);
 
 /// create raisim objects
-  auto ground = world.addGround();
+  world.addGround(0, "gnd");
 
-  auto robot = world.addArticulatedSystem("/home/yjy/github/raisimLib/build/examples/rsc/anymal/urdf/anymal.urdf");
+  auto robot = world.addArticulatedSystem("/home/yjy/JYPro/urdf/JYPro_ocs2.urdf");
     std::cout << "add Done" << std::endl;
   robot->setName("jypro");
   Eigen::VectorXd jointNominalConfig(robot->getGeneralizedCoordinateDim()), jointVelocityTarget(robot->getDOF());
-  jointNominalConfig.setZero(); jointVelocityTarget.setZero();
+  jointNominalConfig << 0, 0, 0.54, 1.0, 0.0, 0.0, 0.0, 0.0, -0.87, 1.78, -0.0, -0.87, 1.78, 0.0, -0.87, 1.78, -0.0, -0.87, 1.78;
+  jointVelocityTarget.setZero();
 
   std::cout << "sime of coordinate: " << robot->getGeneralizedCoordinateDim() << std::endl;
   std::cout << "sime of DOF: " << robot->getDOF() << std::endl;
 //  raisim::Vec<4> quat; quat = {0, 0.0499792, 0, 0.9987503}; quat/= quat.norm();
 //   gc.segment<7>(0) << 0, 0, 0.197, 1, 0, 0, 0;
   
+  Eigen::VectorXd jointPgain(robot->getDOF()), jointDgain(robot->getDOF());
+  jointPgain.tail(12).setConstant(100.0);
+  jointDgain.tail(12).setConstant(1.0);
 
   robot->setGeneralizedCoordinate(jointNominalConfig);
   robot->setGeneralizedVelocity(jointVelocityTarget);
-//   robot->setGeneralizedForce({0, 0, 0, 0, 0, 0, 30, 30, 30, 30});
-  robot->setIntegrationScheme(raisim::ArticulatedSystem::IntegrationScheme::SEMI_IMPLICIT);
-  robot->setControlMode(raisim::ControlMode::FORCE_AND_TORQUE);
+  robot->setPdGains(jointPgain, jointDgain);
+  robot->setPdTarget(jointNominalConfig, jointVelocityTarget);
+  // robot->setControlMode(raisim::ControlMode::FORCE_AND_TORQUE);
 
   /// launch raisim server
   raisim::RaisimServer server(&world);
@@ -37,7 +41,7 @@ int main(int argc, char* argv[]) {
   server.focusOn(robot);
 
   for (int i=0; i<20000000; i++) {
-    std::this_thread::sleep_for(std::chrono::microseconds(10000));
+    std::this_thread::sleep_for(std::chrono::microseconds(10));
     server.integrateWorldThreadSafe();
   }
 
