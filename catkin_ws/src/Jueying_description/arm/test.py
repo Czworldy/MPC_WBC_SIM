718e8081039ba0c373aac3d6aecfb34e945fc111
Czworldy <351123845@qq.com>
2024-07-14 23:42:45 +0800

    add pronto-base est in sim, fix some zyx and xyz eular bugs in global planner. MPC_WBC_sim rm catkin_ws, X20_rsm add imu sensor

--- catkin_ws/src/Jueying_description/arm/test.py
+++
@@ -1,9 +0,0 @@
-import pybullet as p
-
-physicsClient = p.connect(p.GUI)
-p.setGravity(0,0,-9.8)
-arm = p.loadURDF("/model/arm.urdf")
-p.resetJointState(arm, 4, 0.5)
-
-while 1:
-    a = 1
--- catkin_ws/src/Jueying_description/jy_estimate_test/pronto/pronto_quadruped_ros/src/foot_sensor_stance_detector_ros.cpp
+++
@@ -1,1 +0,0 @@
-#include "pronto_quadruped_ros/foot_sensor_stance_detector_ros.hpp"
